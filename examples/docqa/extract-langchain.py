e5b8a811486fc2e2548ac3e5d62ce81aea8d8e2a
Prasad Chalasani <pchalasani@gmail.com>
2023-11-01 10:08:13 -0400

    updated extract examples

--- examples/docqa/extract-langchain.py
+++ examples/docqa/extract-langchain.py
@@ -9,6 +9,10 @@ PRE-REQUISITE: install langchain in your virtual environment.
 Run like this, from project root:
 $ python3 examples/docqa/extract-langchain.py
 
+Optional arguments:
+    --path=... : path to text file to extract from (default is examples/docqa/giraffes.txt)
+    --query="..." : query to run (default is "What do we know about giraffes?")
+    --split=False: whether to split the text into paragraphs (default is False)
 '''
 
 from dotenv import load_dotenv
@@ -18,41 +22,44 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema.document import Document
 from langchain.retrievers.document_compressors import LLMChainExtractor
 from langchain.callbacks import get_openai_callback
-import pip
+from fire import Fire
 
 
 load_dotenv()
 
-path = "examples/docqa/giraffes.txt"
-with open(path, "r") as f:
-    text = f.read()
-texts = text.split("\n\n")
-texts = [
-    Document(page_content=t) for t in texts
-]
-texts = [Document(page_content=text)]
+def main(path="examples/docqa/giraffes.txt", query="What do we know about giraffes?", split=False):
+    with open(path, "r") as f:
+        text = f.read()
+    texts = text.split("\n\n")
+    if split:
+        texts = [
+            Document(page_content=t) for t in texts
+        ]
+    else:
+        texts = [Document(page_content=text)]
 
 
-llm = ChatOpenAI(temperature=0, model_name="gpt-4")
-extractor = LLMChainExtractor.from_llm(llm)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+    extractor = LLMChainExtractor.from_llm(llm)
 
-query = "What do we know about giraffes?"
+    start = time()
 
-start = time()
+    with get_openai_callback() as cb:
+        compressed_docs = asyncio.run(extractor.acompress_documents(texts, query, [cb]))
 
-with get_openai_callback() as cb:
-    compressed_docs = asyncio.run(extractor.acompress_documents(texts, query, [cb]))
+    end = time()
 
-end = time()
+    print(cb)
 
-print(cb)
+    print(f"Time (secs): {end - start}")
 
-print(f"Time (secs): {end - start}")
+    content = "\n".join(d.page_content for d in compressed_docs)
+    print("Extracted content:")
+    print(content)
+    
+    n_sentences = len(content.split("."))
+    tot_sentences = len(text.split("."))
+    print(f"Extracted {n_sentences} sentences out of {tot_sentences}")
 
-content = "\n".join(d.page_content for d in compressed_docs)
-
-n_sentences = len(content.split("."))
-tot_sentences = len(text.split("."))
-print(f"Extracted {n_sentences} sentences out of {tot_sentences}")
-
-
+if __name__ == "__main__":
+    Fire(main)
--- examples/docqa/extract-langroid.py
+++ examples/docqa/extract-langroid.py
@@ -9,11 +9,17 @@ Ensure you have OPENAI_API_KEY=... set in your .env file in project root.
 Run like this, from project root:
 $ python3 examples/docqa/extract-langroid.py
 
+Optional arguments:
+    --path=... : path to text file to extract from (default is examples/docqa/giraffes.txt)
+    --query="..." : query to run (default is "What do we know about giraffes?")
+    --split=False: whether to split the text into paragraphs (default is False)
+
 '''
 
 import langroid as lr
 from time import time
 from langroid.utils.configuration import set_global, Settings
+from fire import Fire
 
 DocChatAgentConfig = lr.agent.special.DocChatAgentConfig
 DocChatAgent = lr.agent.special.DocChatAgent
@@ -26,37 +32,37 @@ DocMetaData = lr.mytypes.DocMetaData
 set_global(Settings(cache=False, debug=False))
 
 
-# configure agent
-path = "examples/docqa/giraffes.txt"
+def main(path="examples/docqa/giraffes.txt", query="What do we know about giraffes?", split=False):
+    # configure agent
 
-# read text from path
-with open(path, "r") as f:
-    text = f.read()
+    # read text from path
+    with open(path, "r") as f:
+        text = f.read()
+    if split:
+        docs = [
+            Document(content=t, metadata=DocMetaData(source="user")) for t in text.split("\n\n")
+        ]
+    else:
+        # or pass the entire text as a single document
+        docs = [Document(content=text, metadata=DocMetaData(source="user"))]
 
-docs = [
-    Document(content=t, metadata=DocMetaData(source="user")) for t in text.split("\n\n")
-]
-# or pass the entire text as a single document
-docs = [Document(content=text, metadata=DocMetaData(source="user"))]
+    agent_cfg = DocChatAgentConfig()
 
-agent_cfg = DocChatAgentConfig()
+    agent = DocChatAgent(agent_cfg)
 
-agent = DocChatAgent(agent_cfg)
+    start = time()
+    extracts = agent.get_verbatim_extracts(query, docs)
+    end = time()
+    content = "\n".join([e.content for e in extracts])
+    n_sentences = len(content.split("."))
+    tot_sentences = len(text.split("."))
 
+    print("Extracted content:")
+    print(content)
+    print(f"Extracted {n_sentences} sentences out of {tot_sentences}")
+    print(f"Time (secs): {end - start}")
+    print(str(lr.language_models.base.LanguageModel.usage_cost_summary()))
 
-query = "What do we know about giraffes?"
-start = time()
-extracts = agent.get_verbatim_extracts(query, docs)
-end = time()
-content = "\n".join([e.content for e in extracts])
-n_sentences = len(content.split("."))
-tot_sentences = len(text.split("."))
-print(f"Extracted {n_sentences} sentences out of {tot_sentences}")
+if __name__ == "__main__":
+    Fire(main)
 
-
-print(f"Time (secs): {end - start}")
-
-print(str(lr.language_models.base.LanguageModel.usage_cost_summary()))
-
-
-# agent.get_summary_answer(query, extracts)
