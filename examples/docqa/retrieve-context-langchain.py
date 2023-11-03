32a778c80df87efcea2ea652f5d4c85b9abb7d7a
Prasad Chalasani <pchalasani@gmail.com>
2023-11-03 15:36:28 -0400

    docqa: context-retrieval examples

---
+++ examples/docqa/retrieve-context-langchain.py
@@ -0,0 +1,113 @@
+'''
+Answer question about a document using LangChain's
+Parent Document Retriever, which
+embeds small chunks and retrieves "parent" chunks.
+
+Ensure you have OPENAI_API_KEY=... set in your .env file in project root.
+
+PRE-REQUISITE: install langchain in your virtual environment.
+
+Run like this, from project root:
+$ python3 examples/docqa/retrieve-context-langchain.py
+
+Optional arguments:
+    --path=... : path to text file to extract from (default is examples/docqa/giraffes.txt)
+    --query="..." : query to run (default is "What do we know about giraffes?")
+
+Also see this colab:
+https://colab.research.google.com/drive/1JvH6CO9AS7CaWK0GTblZGesJoo9Jjyn7
+
+'''
+
+from dotenv import load_dotenv
+from time import time
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.retrievers import ParentDocumentRetriever
+
+## Text Splitting & Docloader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.storage import InMemoryStore
+from langchain.document_loaders import TextLoader
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.retrievers import ContextualCompressionRetriever
+
+from langchain.callbacks import get_openai_callback
+from fire import Fire
+
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+
+model_name = "BAAI/bge-large-en-v1.5"
+#model_name = "BAAI/bge-small-en-v1.5"
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+
+bge_embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs={'device': 'cpu'},
+    encode_kwargs=encode_kwargs
+)
+
+load_dotenv()
+
+def main(
+        path="examples/docqa/employment.txt",
+        #query= "What are the provisions following cessation of employment?",
+        query="What must an employee do when their role changes?",
+):
+    loader = TextLoader(path)
+    docs = loader.load()
+
+    # This text splitter is used to create the parent documents - The big chunks
+    parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
+
+    # This text splitter is used to create the child documents - The small chunks
+    # It should create documents smaller than the parent
+    child_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
+
+    # The vectorstore to use to index the child chunks
+    vectorstore = Chroma(collection_name="split_parents", embedding_function=bge_embeddings) #OpenAIEmbeddings()
+
+    # The storage layer for the parent documents
+    store = InMemoryStore()
+
+    big_chunks_retriever = ParentDocumentRetriever(
+        vectorstore=vectorstore,
+        docstore=store,
+        child_splitter=child_splitter,
+        parent_splitter=parent_splitter,
+    )
+
+    big_chunks_retriever.add_documents(docs)
+    n_keys = len(list(store.yield_keys()))
+    print(f"Indexed {n_keys} chunks")
+
+    sub_docs = vectorstore.similarity_search(query)
+    n_sub_docs = len(sub_docs)
+    print(f"Found {n_sub_docs} sub-documents")
+
+    retrieved_docs = big_chunks_retriever.get_relevant_documents(query)
+    n_retrieved_docs = len(retrieved_docs)
+    print(f"Retrieved {n_retrieved_docs} parent documents")
+
+    from langchain.chains import RetrievalQA
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+
+    compressor = LLMChainExtractor.from_llm(llm)
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor, base_retriever=big_chunks_retriever,
+    )
+    qa = RetrievalQA.from_chain_type(llm=llm,
+                                     chain_type="stuff",
+                                     retriever=compression_retriever)
+    start = time()
+    with get_openai_callback() as cb:
+        answer = qa.run(query)
+    print(f"Answer: {answer}")
+    end = time()
+
+    print(f"Time (secs): {end - start}")
+    print(cb)
+
+if __name__ == "__main__":
+    Fire(main)
---
+++ examples/docqa/retrieve-context-langroid.py
@@ -0,0 +1,95 @@
+'''
+Answer a question about a document using Langroid's DocChatAgent,
+which uses Dynamic Context Retrieval (DCR) to retrieve a desired window of
+chunks around the matching chunk.
+
+Ensure you have followed the install instructions in the README.md file.
+Essentially, have a virtual env and do `pip install langroid`.
+Ensure you have OPENAI_API_KEY=... set in your .env file in project root.
+
+Run like this, from project root:
+$ python3 examples/docqa/retrieve-context-langroid.py
+
+Optional arguments:
+    --path=... : path to text file to extract from (default is examples/docqa/giraffes.txt)
+    --query="..." : query to run (default is "What do we know about giraffes?")
+    --k=... : number of neighbor chunks to retrieve (default is 2)
+
+Also see this colab:
+https://colab.research.google.com/drive/1JvH6CO9AS7CaWK0GTblZGesJoo9Jjyn7
+'''
+
+import langroid as lr
+from time import time
+from langroid.utils.configuration import set_global, Settings
+from fire import Fire
+
+DocChatAgentConfig = lr.agent.special.DocChatAgentConfig
+DocChatAgent = lr.agent.special.DocChatAgent
+ParsingConfig = lr.parsing.parser.ParsingConfig
+QdrantDBConfig = lr.vector_store.QdrantDBConfig
+Document = lr.mytypes.Document
+ChatDocument = lr.agent.chat_document.ChatDocument
+DocMetaData = lr.mytypes.DocMetaData
+
+set_global(Settings(cache=False, debug=False))
+
+
+def main(
+        path="examples/docqa/employment.txt",
+        #query= "What are the provisions following cessation of employment?",
+        query="What must an employee do when their role changes?",
+        k=2,
+):
+    # configure agent
+
+    # read text from path
+    with open(path, "r") as f:
+        text = f.read()
+    docs = [Document(content=text, metadata=DocMetaData(source="user"))]
+
+    agent_cfg = DocChatAgentConfig(
+        n_neighbor_chunks=k,
+        vecdb=QdrantDBConfig(
+            collection_name="testing",
+            replace_collection=True,
+        ),
+        parsing=ParsingConfig(
+            splitter=lr.parsing.parser.Splitter.TOKENS,
+            chunk_size=200,
+            overlap=70,
+            separators=["."],
+            n_neighbor_ids=5,
+            n_similar_docs=3,
+        )
+    )
+
+    agent = DocChatAgent(agent_cfg)
+    agent.ingest_docs(docs)
+
+
+    # clear history and usage
+    agent.clear_history(0)
+    agent.llm.reset_usage_cost()
+
+    start = time()
+
+    chunks = agent.get_relevant_chunks(query)
+    end = time()
+    content = "\n".join([c.content for c in chunks])
+    n_words = len(content.split(" "))
+    tot_words = len(text.split(" "))
+
+    print(f"Retrieved chunks: {n_words} words out of {tot_words}")
+
+
+    extracts = agent.get_verbatim_extracts(query, chunks)
+    answer = agent.get_summary_answer(query, extracts)
+    print(f"Answer: {answer}\n")
+
+    print(f"Time (secs): {end - start}")
+    print(str(lr.language_models.base.LanguageModel.usage_cost_summary()))
+
+if __name__ == "__main__":
+    Fire(main)
+
