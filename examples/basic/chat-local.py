abcd5b25da31f9996416d47c4ce08859a0032ca2
Prasad Chalasani <pchalasani@gmail.com>
2024-01-01 19:03:17 -0500

    examples/docqa/rag-local-simple.py

--- examples/basic/chat-local.py
+++ examples/basic/chat-local.py
@@ -15,10 +15,17 @@ Then run this script as follows:
 import langroid as lr
 import langroid.language_models as lm
 
-# assume you've run `ollama pull mistral` to spin up `mistral` locally
+# Assume you've run `ollama pull mistral` to spin up `mistral` locally.
+# Notes:
+# - we use `lm.OpenAIGPTConfig` to incidate this config is for LLMs served
+#    at OpenAI-compatible endpoints)
+# - if you omit `chat_model` below, it defaults to OpenAI GPT4-turbo,
+#   or you can explicitly specify it as `lm.OpenAIChatModel.GPT4` or `lm.OpenAIChatModel.GPT4_TURBO`
+
 llm_config = lm.OpenAIGPTConfig(
-    chat_model="litellm/ollama/mistral",
-    chat_context_length=2048,  # adjust based on your local LLM params
+    chat_model="local/api-inference.huggingface.co/models/TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    max_output_tokens=20,
+    chat_context_length=500,  # adjust based on your local LLM params
 )
 
 # Alternatively, if you've used ooba or other lib to spin up a Local LLM
@@ -27,7 +34,7 @@ llm_config = lm.OpenAIGPTConfig(
 # llm_config = lm.OpenAIGPTConfig(
 #     chat_model="local/localhost:8000"
 # )
-# If the endpoint is listening at https://localhost:8000, you must include the `v1`
+# If the endpoint is listening at https://localhost:8000/v1, you must include the `v1`
 # at the end, e.g. chat_model="local/localhost:8000/v1"
 
 agent_config = lr.ChatAgentConfig(
---
+++ examples/docqa/rag-local-simple.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import langroid as lr
+import langroid.language_models as lm
+from langroid.agent.special.doc_chat_agent import DocChatAgent, DocChatAgentConfig
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+# (1) Mac: Install latest ollama, then do this:
+# ollama pull mistral:7b-instruct-v0.2-q4_K_M
+
+# (2) Ensure you've installed the `litellm` extra with Langroid, e.g.
+# pip install langroid[litellm], or if you use the `pyproject.toml` in this repo
+# you can simply say `poetry install`
+
+llm = lm.OpenAIGPTConfig(
+    chat_model="litellm/ollama/mistral:7b-instruct-v0.2-q4_K_M",
+    completion_model="litellm/ollama/mistral:7b-instruct-v0.2-q4_K_M",
+    chat_context_length=4096, # set this based on model
+    max_output_tokens=100,
+    temperature=0.2,
+    stream=True,
+    timeout=45,
+)
+
+# test if basic chat works with this llm setup
+#
+# agent = lr.ChatAgent(
+#     lr.ChatAgentConfig(
+#         llm=llm
+#     )
+# )
+#
+# agent.llm_response("What is 3 + 4?")
+#
+# task = lr.Task(agent)
+# task.run("Concisely answer some questions")
+
+hf_embed_config = lr.embedding_models.SentenceTransformerEmbeddingsConfig(
+    model_type="sentence-transformer",
+    model_name="BAAI/bge-large-en-v1.5",
+)
+
+config = DocChatAgentConfig(
+    default_paths= [],
+    conversation_mode=True,
+    llm = llm,
+    relevance_extractor_config = lr.agent.special.RelevanceExtractorAgentConfig(
+        llm=llm
+    ),
+    vecdb=lr.vector_store.QdrantDBConfig(
+        collection_name="test1",
+        replace_collection=True,
+        embedding=hf_embed_config
+    ),
+    doc_paths= [
+        "https://arxiv.org/pdf/2312.17238.pdf",
+    ],
+    system_message="""
+    Answer some questions about docs. Be concise. 
+    Start by asking me what I want to know
+    """,
+)
+
+agent = DocChatAgent(config)
+task = lr.Task(agent)
+task.run()
+
+
+
